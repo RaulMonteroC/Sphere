45471d440d5803d478fb9f49a838ce9d3c0ba563
RaulMonteroC <raulmonteroc01@gmail.com>
2015-10-13 00:26:13 -0400

    Created ContextRepository class structure which is a repository implementation on a sql data store using entity framework.

---
+++ Sphere.Core/ContextRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace Sphere.Core
+{
+    public class ContextRepository<T> : Repository<T> where T : class
+    {
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Func<T, bool> condition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Exec(string query, params SqlParameter[] sqlParameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> Find(Func<T, bool> condition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(Func<T, bool> condition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Run<TEntity>(string query, params SqlParameter[] sqlParameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
