232c9c8e9118c441f26cf8ec4c256f07432fe174
RaulMonteroC <raulmonteroc01@gmail.com>
2015-10-13 00:19:11 -0400

    - Changed project type from portable class library (PCL) to standard class library. - Created repository interface.

---
+++ Sphere.Core/Repository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace Sphere.Core
+{
+    public interface Repository<T> where T : class
+    {
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(Func<T, bool> condition);
+
+        T Get(Func<T, bool> condition);
+        IQueryable<T> GetAll();
+        IQueryable<T> Find(Func<T, bool> condition);
+
+        void Exec(string query, params SqlParameter[] sqlParameters);
+        TEntity Run<TEntity>(string query, params SqlParameter[] sqlParameters);
+    }
+}
